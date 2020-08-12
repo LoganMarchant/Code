47b71646242d9d6a801caa7fc031181d8058919a
lmarc5671 <lmarc5671@mtroyal.ca>
2020-08-12 01:26:53 -0600

    Create code.java

---
+++ code.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      boolean a = true;
+        if (a==true) {
+            System.out.println(a);
+        } else {
+            System.out.println(a);
+        }
+    }
+  }
